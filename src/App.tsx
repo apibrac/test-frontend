c84c4af302d9aa6954ef99058c080472daf84032
Chouteau <tetyana.chouteau@passculture.app>
2023-02-05 10:06:58 +0100

    Fetch pour news json

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import "./App.css";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo-pass-culture.svg";
 
+//import dataNews from "./news.json";
+
 function App() {
+  const [dataNews, setDataNews] = useState({ news: [] });
+
+  useEffect(() => {
+    fetch("data/news.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setDataNews(data);
+
+        console.log("fetch");
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        {dataNews.news.map((el, index) => {
+          return <h1 key={index}>{el["title"]}</h1>;
+        })}
+      </main>
     </div>
   );
 }
