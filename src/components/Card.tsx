b3139d610a288247f24020a6ab8170a0ee5aac38
Chouteau <tetyana.chouteau@passculture.app>
2023-02-05 10:43:22 +0100

    Design card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,17 +2,15 @@ import "./Card.css";
 
 function Card() {
   return (
-    <div className="card">
-      <figure>
-        <img src="/images/cannes1.jpg" alt="TITRE" />
-        <figcaption>
-          <label className="category">EVENEMENT</label>
-          <label className="dtae">12/12/2023</label>
-          <h2>Titre</h2>
-          <p>lalalalalalalalalla</p>
-        </figcaption>
-      </figure>
-    </div>
+    <figure className="card">
+      <img src="./images/cannes1.jpg" alt="TITRE" />
+      <figcaption>
+        <label className="category">EVENEMENT</label>
+        <label className="date">12/12/2023</label>
+        <h2>Titre</h2>
+        <p>lalalalalalalalalla</p>
+      </figcaption>
+    </figure>
   );
 }
 export default Card;
