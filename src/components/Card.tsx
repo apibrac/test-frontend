b2044ac7783b328b0cdf224e79e57b6c8a748b79
Chouteau <tetyana.chouteau@passculture.app>
2023-02-06 10:34:42 +0100

    Ajout card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,22 @@
 import "./Card.css";
 
-function Card() {
+interface CardProps {
+  title: string;
+  category: string;
+  date: string;
+  images: string[];
+  text: string;
+}
+
+function Card(props: CardProps) {
   return (
     <figure className="card">
-      <img src="./images/cannes1.jpg" alt="TITRE" />
+      <img src={props.images[0]} alt={props.title} />
       <figcaption>
-        <label className="category">EVENEMENT</label>
-        <label className="date">12/12/2023</label>
-        <h2>Titre</h2>
-        <p>lalalalalalalalalla</p>
+        <label className="category">{props.category}</label>
+        <label className="date">{props.date}</label>
+        <h2>{props.title}</h2>
+        <p>{props.text}</p>
       </figcaption>
     </figure>
   );
