d7bb71e18be847eba2004534cac34143e7699649
Chouteau <tetyana.chouteau@passculture.app>
2023-02-08 08:34:20 +0100

    Ajout photo emply dans srs de Card; Ajout select sans text d'option

--- src/App.tsx
+++ src/App.tsx
@@ -23,18 +23,42 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <main>
-        {dataNews.news.map((el, index) => {
-          return (
-            <Card
-              key={index}
-              title={el["title"]}
-              category={el["category"]}
-              date={el["date"]}
-              images={el["images"]}
-              text={el["text"]}
-            ></Card>
-          );
-        })}
+        <div id="select">
+          <label htmlFor="pet-select">Choose a pet:</label>
+          <select name="pets" id="pet-select">
+            <option value="">--Please choose an option--</option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="hamster">Hamster</option>
+            <option value="parrot">Parrot</option>
+            <option value="spider">Spider</option>
+            <option value="goldfish">Goldfish</option>
+          </select>
+          <label htmlFor="pet-select">Choose a pet:</label>
+          <select name="pets" id="pet-select">
+            <option value="">--Please choose an option--</option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="hamster">Hamster</option>
+            <option value="parrot">Parrot</option>
+            <option value="spider">Spider</option>
+            <option value="goldfish">Goldfish</option>
+          </select>
+        </div>
+        <div id="news">
+          {dataNews.news.map((el, index) => {
+            return (
+              <Card
+                key={index}
+                title={el["title"]}
+                category={el["category"]}
+                date={el["date"]}
+                images={el["images"]}
+                text={el["text"]}
+              ></Card>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
