96920837d31ad0edeed130c31041ce8c180d0bae
Chouteau <tetyana.chouteau@passculture.app>
2023-02-27 11:28:31 +0100

    Tdd: all news figure, cat Ambassadeurs.

--- src/components/News.test.jsx
+++ src/components/News.test.jsx
@@ -2,7 +2,11 @@ import * as React from "react";
 // add news
 import News from "./News";
 // **** Nécessaire à la librairie de test *****
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+// **** Nécessaire au action utilisateur
+import userEvent from "@testing-library/user-event";
+// on récupère le json des news
+import json from "../../public/data/news.json";
 
 //1. should display all news:
 //render le composant
@@ -19,63 +23,65 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 //utilisateur remet le bouton select sur toutes les catégories
 //expect compter le nombre de news
 
-describe("News selector", () => {
-  const props = { onSelectNews: {
-jest.fn() };
+beforeEach(() => {
+  jest.spyOn(global, "fetch").mockResolvedValue({
+    json: jest.fn().mockResolvedValue(json),
+  });
+});
 
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+describe("News selector", () => {
   it("should display all news", async () => {
-    // ***** On demande à executer la balise News mais dans le router car sinon il y a une erreur sur usenavigate ****
-    render(
-        <News />
-    );
+    // ***** On demande à executer la balise News
+    render(<News />);
+    // *** Le test ici c'est de chercher le tag h2 (créer par les news) ***
+    // *** Il doit y avoir 7 h2 car il y a 7 nouvelles dans le json ***
     await waitFor(() => {
-      // *** Le test ici c'est de chercher le tag figure (créer par les news) ***
-      // *** Il doit y avoir 7 figures car il y a 7 nouvelles dans le json ***
       expect(screen.getAllByRole("figure")).toHaveLength(7);
     });
+  });
 
-  it("should display news in xxx category when user selects filter", async () => {
-      await render(
-          <News />
-      );
+  it("should display news in Ambassadeurs category when user selects filter", async () => {
+    render(<News />);
 
-      // *** on lance un evenement qui va changer la combo qui s'appelle catégorie
-      // *** et qui prend la valeur Ambassadeur comme si un utilisateur faisait ça
-      await fireEvent.change(
-        screen.getByRole("combobox", { name: "Catégorie:" }),
-        { target: { value: "Ambassadeurs" } }
-      );
+    // *** on lance un evenement qui va changer la combo qui s'appelle catégorie
+    // *** et qui prend la valeur Ambassadeur comme si un utilisateur faisait ça
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Catégorie:" }),
+      "Ambassadeurs"
+    );
 
-      await waitFor(
-        () => {
-          // *** cette fois il n'y a que 2 nouvelles qui on la catégorie Ambassadeur
-          expect(screen.getAllByRole("figure")).toHaveLength(2);
-        },
-        {
-          timeout: 4000,
-        }
-      );
+    // *** cette fois il n'y a que 2 nouvelles qui on la catégorie Ambassadeur
+    await waitFor(() => {
+      expect(screen.getAllByRole("figure")).toHaveLength(2);
+    });
+  });
 
   it("should display all news when user resets filter", async () => {
-      await render(
-          <News />
-      );
-      // **** Même chose que le test d'au dessus mais on remet le choix à vide (donc pas de filtrage)
-      await fireEvent.change(
-        screen.getByRole("combobox", { name: "Catégorie:" }),
-        { target: { value: "" } }
-      );
-      await waitFor(
-        () => {
-          expect(
-            // *** cette fois on doit encore avoir 7 figure.
-            screen.getAllByRole("figure")
-          ).toHaveLength(7);
-        },
-        {
-          timeout: 4000,
-        }
-      );
+    render(<News />);
+    // **** Même chose que le test d'au dessus mais on remet ensuite le choix à vide (donc pas de filtrage)
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Catégorie:" }),
+      "Ambassadeurs"
+    );
+    await waitFor(() => {
+      expect(
+        // *** cette fois on doit encore avoir 7 figure.
+        screen.getAllByRole("figure")
+      ).toHaveLength(2);
     });
-  };
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Catégorie:" }),
+      ""
+    );
+    await waitFor(() => {
+      expect(
+        // *** cette fois on doit encore avoir 7 figure.
+        screen.getAllByRole("figure")
+      ).toHaveLength(7);
+    });
+  });
 });
